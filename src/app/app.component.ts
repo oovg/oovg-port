5d5b9110198a223b877d465559e6b96a65f25d4c
oovg <office@vengist.com>
2016-12-27 14:51:39 -0800

    move styles to folder. begin grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ActivatedRouteSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-root',
