b26c5d03e84f608117affcef630276baaee01bf6
oovg <office@vengist.com>
2017-01-04 12:25:37 -0800

    adds lastScrollTop and scroll position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { routerTransition } from './router.animations';
 
 @Component({
@@ -9,5 +9,22 @@ import { routerTransition } from './router.animations';
   host: {'[@routerTransition]': ''}
 })
 export class AppComponent {
+  lastScrollTop: number = 0;
+  direction: string = "";
 
+  constructor(lc: NgZone) {
+     window.onscroll = () => {
+        let st = window.pageYOffset;
+        let dir = '';
+        if (st > this.lastScrollTop) {
+            dir = "down";
+        } else {
+            dir = "up";
+        }
+        this.lastScrollTop = st;
+        lc.run(() => {
+          this.direction = dir;
+        });
+      };
+  }
 }
